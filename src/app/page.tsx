0e5f95113b3765d309a62eea007c40922532ad07
coolfin <sorlti6952@gmail.com>
2024-07-16 14:42:09 +0900

    fix: 모바일 view 높이 단위 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Link from 'next/link'
-
 export default function Home() {
   return (
     <div>
       <h1>SCON</h1>
       <p>당신만의 스테이지, 스콘이 연결합니다.</p>
 
-      <Link href="/login">로그인 페이지로 이동</Link>
+      <a href="/login">로그인 페이지로 이동</a>
     </div>
   )
 }
