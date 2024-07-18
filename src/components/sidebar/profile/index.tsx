f5fe6bd44b51e867b94c16d99c5449fc5e7c2723
coolfin <sorlti6952@gmail.com>
2024-07-18 15:41:08 +0900

    design: chevron 아이콘 추가

--- src/components/sidebar/profile/index.tsx
+++ src/components/sidebar/profile/index.tsx
@@ -1,3 +1,4 @@
+import ArrowRightSVG from '@/static/svg/chevron-right.svg'
 import OvenMarkSVG from '@/static/svg/ovner-mark.svg'
 import ProfileSVG from '@/static/svg/sidebar-profile.svg'
 
@@ -9,12 +10,14 @@ interface SideBarProfileProps {
 function SideBarProfile(props: SideBarProfileProps) {
   const { nickname, isOven } = props
   return (
-    <div className="px-7 py-4 flex items-center gap-x-12 text-2xl bg-[#E5E5ED] rounded-xl">
+    <div className="px-7 py-4 flex items-center justify-between gap-x-12 text-2xl bg-[#E5E5ED] rounded-xl">
       <ProfileSVG className="w-14 h-14" />
-      <p className="flex gap-x-3 items-center justify-between">
+      <p className="flex flex-1 gap-x-3 items-center justify-start">
         <strong className="font-bold">{nickname}님</strong> 어서오세요!
         {isOven && <OvenMarkSVG className="w-7 h-7" />}
       </p>
+
+      <ArrowRightSVG className="w-8 h-8 cursor-pointer" />
     </div>
   )
 }
