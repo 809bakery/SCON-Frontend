94fd83d65d1a681318ea383453f61e407311322e
kwakseongjae <gkffhdnls13@gmail.com>
2024-07-18 17:26:31 +0900

    fix: 100dvh -> dvh 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
       </head>
       <body
-        className={`${pretendard.className} w-full h-100dvh flex justify-center items-center `}
+        className={`${pretendard.className} w-full h-dvh flex justify-center items-center`}
       >
         <div className="w-full max-w-[600px] sm:border-l-2 sm:border-r-2 border-none border-[#f9f9f9]">
           <Provider>{children}</Provider>
