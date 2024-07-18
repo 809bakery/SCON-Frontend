587aa600273f53f0fa4da1e02bb47af77a921c98
곽성재 <87296259+kwakseongjae@users.noreply.github.com>
2024-07-18 17:19:05 +0900

    update: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -13,9 +14,26 @@ const config: Config = {
         warning: '#F85252',
         success: '#489D59',
         disabled: '#6B6E78',
+        border: '#D6D5D5',
+        yellow: '#FFF0BB',
+      },
+      borderWidth: {
+        '0.5': '.0313rem',
+        '2': '.125rem',
+      },
+      width: {
+        '30': '7.5rem',
+      },
+      height: {
+        '15': '3.75rem',
+      },
+      gap: {
+        '23': '5.75rem',
+      },
+      margin: {
+        '49': '12.25rem',
       },
     },
   },
-  plugins: [],
 }
 export default config
