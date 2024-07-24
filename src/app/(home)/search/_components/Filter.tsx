6f8c0ae156e286bddc1ceae9219af2142f20059a
kwakseongjae <gkffhdnls13@gmail.com>
2024-07-24 10:38:31 +0900

    design: 검색 페이지 필터 버튼 UI 수정

--- src/app/(home)/search/_components/Filter.tsx
+++ src/app/(home)/search/_components/Filter.tsx
@@ -39,21 +39,21 @@ export default function Filter({ filterQuery, setFilterQuery }: FilterProps) {
     <div ref={filterRef}>
       <div
         role="presentation"
-        className={`w-[5.625rem] h-[1.875rem] border border-primary flex items-center justify-center cursor-pointer pl-1 ${openFilter ? 'rounded-t' : 'rounded'}`}
+        className={`w-[7.5rem] h-[2.5rem] text-base leading-6 font-bold sm:font-medium border border-primary flex items-center justify-center cursor-pointer pl-1 ${openFilter ? 'rounded-t' : 'rounded'}`}
         onClick={() => setOpenFilter(!openFilter)}
       >
-        <span className="text-xs font-bold">{filterQuery || '판매많은순'}</span>
+        <span className="">{filterQuery || '판매많은순'}</span>
         <ArrowRight className="w-4 h-4 rotate-90 mb-[.125rem]" />
       </div>
       <div
-        className={`flex flex-col absolute top-[3.375rem] bg-white  ${!openFilter && 'hidden'}`}
+        className={`flex flex-col absolute top-16 bg-white  ${!openFilter && 'hidden'}`}
       >
         {openFilter &&
           FilterList.map((filter) => (
             <button
               type="button"
               key={filter.id}
-              className={`w-[5.625rem] h-[1.875rem] text-xs font-bold flex items-center justify-center cursor-pointer px-[.375rem] ${filter.id === 1 ? 'border-r-0.5 border-l-0.5 border-b-0.5' : 'border-0.5'} border-border hover:bg-lightgray-1`}
+              className={`w-[7.5rem] h-[2.5rem] font-bold sm:font-medium flex leading-6 items-center justify-center cursor-pointer px-[.375rem] ${filter.id === 1 ? 'border-r-0.5 border-l-0.5 border-b-0.5' : 'border-0.5'} border-border hover:bg-lightgray-1`}
               onClick={() => handleChangeFilter(filter.label)}
             >
               {filter.label}
