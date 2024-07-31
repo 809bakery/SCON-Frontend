76a597b9600ebcf21e4328d1e06bfba6e02e9bf7
coolfin <sorlti6952@gmail.com>
2024-07-31 10:54:51 +0900

    feat: 예매 페이지 캘린더 구성

---
+++ src/app/ticket/[id]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import TicketCalendar from '@/features/ticket/calendar/index.tsx'
+
+function TicketBookPage() {
+  return (
+    <div className="w-full px-7 py-3">
+      <TicketCalendar />
+    </div>
+  )
+}
+
+export default TicketBookPage
---
+++ src/features/ticket/calendar/index.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Calendar from 'react-calendar'
+
+import { DUMMY_STAGE_DETAIL } from '@/constants/stage/index.ts'
+
+type ValuePiece = Date | null
+type Value = ValuePiece | [ValuePiece, ValuePiece]
+
+function TicketCalendar() {
+  const [clickedDate, setClickedDate] = useState<Value>()
+  const [stageList, setStageList] = useState<Date[]>()
+
+  useEffect(() => {
+    setStageList(
+      DUMMY_STAGE_DETAIL.content.map((stage) => new Date(stage.time)),
+    )
+
+    setClickedDate(new Date(DUMMY_STAGE_DETAIL.content[0].time))
+  }, [])
+
+  const handleDisabledDate = ({ date }: { date: Date }) => {
+    if (stageList) {
+      return !stageList.some((stage) => {
+        const year1 = date.getFullYear()
+        const month1 = date.getMonth()
+        const day1 = date.getDate()
+
+        const year2 = stage.getFullYear()
+        const month2 = stage.getMonth()
+        const day2 = stage.getDate()
+
+        return year1 === year2 && month1 === month2 && day1 === day2
+      })
+    }
+    return false
+  }
+  return (
+    <div className="w-full p-5 border border-border rounded-xl">
+      <Calendar
+        locale="ko-kr"
+        onChange={setClickedDate}
+        tileDisabled={handleDisabledDate}
+        value={clickedDate}
+        next2Label={null}
+        prev2Label={null}
+        calendarType="hebrew"
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        formatDay={(locale, date) =>
+          date.toLocaleDateString('en', { day: 'numeric' })
+        }
+      />
+    </div>
+  )
+}
+
+export default TicketCalendar
