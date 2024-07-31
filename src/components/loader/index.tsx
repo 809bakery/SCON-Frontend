487b8561fc5bd4f90d80bc231f93bea45051ac8f
kwakseongjae <gkffhdnls13@gmail.com>
2024-07-31 15:22:09 +0900

    feat: 로더 컴포넌트 추가

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center py-5">
+      <div className="animate-spin rounded-full h-10 w-10 border-t-[3px] border-r-[3px] border-primary" />
+    </div>
+  )
+}
