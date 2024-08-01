244c0e10155cf4e605ad9be70a428aa668eca2da
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-01 10:53:09 +0900

    design: shadow-box config 추가 및 stage card에 적용

--- src/app/(home)/search/_components/StageCard.tsx
+++ src/app/(home)/search/_components/StageCard.tsx
@@ -15,29 +15,36 @@ export default function StageCard({
   image,
 }: StageCardProps) {
   return (
-    <div className="w-full rounded-xl border-0.5 border-l-0.5 border-border px-5 py-[1.875rem] flex hover:bg-lightgray-1 cursor-pointer">
-      <div className="min-h-[12.5rem] max-h-[12.5rem] min-w-[10rem] max-w-[10rem] relative">
-        <Image src={image} fill className=" object-cover" alt="profile" />
-      </div>
-      <div className="w-full flex flex-col pl-7 gap-5">
-        <div className="flex space-x-3 h-[1.875rem] text-xs font-bold">
-          <div className="bg-primary px-2 py-1 flex justify-center items-center rounded">
-            <span>예매중</span>
-          </div>
-          <div className="border border-primary px-2 py-1 flex justify-center items-center rounded">
-            <span>인기</span>
-          </div>
-          <div className="border border-border px-2 py-1 flex justify-center items-center rounded">
-            <span>공연임박</span>
-          </div>
+    <div className="w-full hover:bg-lightgray-1 cursor-pointer">
+      <div className="w-full rounded-xl flex px-5 py-[1.875rem] shadow-border">
+        <div className="min-h-[12.25rem] max-h-[12.25rem] min-w-[10rem] max-w-[10rem] relative">
+          <Image
+            src={image}
+            fill
+            className=" object-cover rounded-xl"
+            alt="profile"
+          />
         </div>
-        <div className="w-full">
-          <div className="w-full h-[4.375rem] flex items-center">
-            <h2 className="text-2xl font-bold line-clamp-2">{title}</h2>
+        <div className="w-full flex flex-col pl-7 gap-5">
+          <div className="flex space-x-3 h-[1.875rem] text-xs font-bold">
+            <div className="bg-primary px-2 py-1 flex justify-center items-center rounded">
+              <span>예매중</span>
+            </div>
+            <div className="border border-primary px-2 py-1 flex justify-center items-center rounded">
+              <span>인기</span>
+            </div>
+            <div className="border border-border px-2 py-1 flex justify-center items-center rounded">
+              <span>공연임박</span>
+            </div>
           </div>
-          <div className="text-disabled flex flex-col gap-1">
-            <h3 className="text-base font-bold">{location}</h3>
-            <p className="text-xs font-medium mt-1">{time}</p>
+          <div className="w-full">
+            <div className="w-full h-[4.375rem] flex items-center">
+              <h2 className="text-2xl font-bold line-clamp-2">{title}</h2>
+            </div>
+            <div className="text-disabled flex flex-col gap-1">
+              <h3 className="text-base font-bold">{location}</h3>
+              <p className="text-xs font-medium mt-1">{time}</p>
+            </div>
           </div>
         </div>
       </div>
