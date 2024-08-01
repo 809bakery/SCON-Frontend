85cf8853dc8009ead4de1a8a27945eb9817c0dec
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-01 18:04:28 +0900

    feat: 마이페이지 메인 링크 연결 및 프로필 변경 페이지 UI 작업

--- src/app/(users)/mypage/page.tsx
+++ src/app/(users)/mypage/page.tsx
@@ -1,3 +1,54 @@
+'use client'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+import MypageTap from '@/app/(users)/mypage/_components/MypageTap.tsx'
+import { DUMMY_OVEN_DETAIL_DATA } from '@/constants/dummy.ts'
+import EditSVG from '@/static/svg/edit-icon.svg'
+
+const name = '주르르'
+
 export default function Mypage() {
-  return <div>SCON 마이페이지</div>
+  const router = useRouter()
+  const user = DUMMY_OVEN_DETAIL_DATA.find((data) => data.ovenName === name)
+
+  return (
+    <div className="flex flex-col">
+      {/* 유저 프로필 */}
+      <div className="w-full flex flex-col pt-5 pb-[3rem] bg-_white">
+        <div className="flex flex-col items-center gap-5 bg-white">
+          <div className="w-[7.5rem] h-[7.5rem] rounded-full border border-border">
+            <Image
+              src={user?.image ?? '/dummy/dummy-default-profile.jpg'}
+              alt={user?.ovenName ?? 'profile'}
+              width={120}
+              height={120}
+              className="aspect-square object-cover rounded-full"
+            />
+          </div>
+          <div className="relative">
+            <span className="font-bold text-2xl leading-8">
+              {user?.ovenName}
+            </span>
+            <div
+              role="presentation"
+              className="w-8 h-8 absolute bottom-0 -right-10  cursor-pointer flex justify-center items-center"
+              onClick={() => router.push('/mypage/profile')}
+            >
+              <EditSVG className="w-4 h-4" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full bg-white border-b divide-y divide-solid">
+        <MypageTap text="회원 정보" />
+        <MypageTap text="비밀번호 재설정" url="/login/password_reset" />
+        <MypageTap text="내 오븐 관리" />
+        <MypageTap text="알림 설정" url="/mypage/push_settings" />
+        <MypageTap text="이용 약관" />
+        <MypageTap text="회원 탈퇴" url="/mypage/leave" color="#F85252" />
+      </div>
+    </div>
+  )
 }
---
+++ src/app/(users)/mypage/profile/page.tsx
@@ -0,0 +1,242 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
+'use client'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useRef, useState } from 'react'
+
+import { nicknameRegExp } from '@/constants/regex/index.ts'
+import DefaultProfile from '@/static/img/dummy/profile/default-profile.jpg'
+import Checked from '@/static/svg/checked-icon.svg'
+import XMarkSVG from '@/static/svg/close-circle-icon.svg'
+import Required from '@/static/svg/required-star.svg'
+import UploadSVG from '@/static/svg/upload-icon.svg'
+
+export default function Profile() {
+  const router = useRouter()
+  const [nickname, setNickname] = useState<string>('주르르')
+  const [nicknameErrorMessage, setNicknameErrorMessage] = useState<
+    string | null
+  >(null)
+  const [isNicknameValid, setIsNicknameValid] = useState<boolean>(false)
+  const [isNicknameCheckSuccess, setIsNicknameCheckSuccess] =
+    useState<boolean>(false)
+
+  const [imageUrl, setImageUrl] = useState<string | null>(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+
+    const {
+      target: { files },
+    } = e
+
+    if (files?.length === 0) {
+      return
+    }
+
+    const file = files?.[0]
+    const fileReader = new FileReader()
+    fileReader.readAsDataURL(file as Blob)
+    fileReader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      }: any = finishedEvent
+      setImageUrl(result)
+    }
+    closeModal()
+  }
+
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  // 모달 백그라운드 클릭 시 모달 닫기
+  const handleModalBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      closeModal()
+    }
+  }
+
+  const handleProfileImageChangeClick = () => {
+    closeModal()
+    fileInputRef.current?.click() // 파일 입력 요소를 클릭합니다.
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e
+    setNickname(value)
+    if (nicknameRegExp.test(value)) {
+      setNicknameErrorMessage(null)
+      setIsNicknameValid(true)
+      setIsNicknameCheckSuccess(true)
+    } else if (value.length === 0) {
+      setNicknameErrorMessage(null)
+      setIsNicknameValid(false)
+      setIsNicknameCheckSuccess(false)
+    } else {
+      setNicknameErrorMessage('2~8자의 한글, 영문, 숫자만 입력 가능합니다.')
+      setIsNicknameValid(false)
+      setIsNicknameCheckSuccess(false)
+    }
+  }
+
+  return (
+    <>
+      <div className="w-full px-7 pt-[100px]">
+        {imageUrl ? (
+          <div className="flex justify-center items-center mb-5">
+            <div
+              role="presentation"
+              className="relative cursor-pointer"
+              onClick={showModal}
+            >
+              <div className="relative w-40 h-40  rounded-full overflow-hidden">
+                <Image
+                  src={imageUrl}
+                  alt="profile"
+                  fill
+                  className="object-cover object-center"
+                />
+              </div>
+              <UploadSVG className="absolute bottom-0 right-0" />
+            </div>
+          </div>
+        ) : (
+          <div className="flex justify-center items-center mb-5">
+            <div
+              role="presentation"
+              className="relative w-40 h-40  rounded-full cursor-pointer"
+              onClick={showModal}
+            >
+              <Image
+                src={DefaultProfile}
+                alt="profile"
+                width={160}
+                height={160}
+                layout="fixed"
+                className="rounded-full"
+              />
+              <UploadSVG className="absolute bottom-0 right-0" />
+            </div>
+          </div>
+        )}
+        <input
+          ref={fileInputRef}
+          type="file"
+          id="file-input"
+          accept="image/*"
+          className="hidden"
+          onChange={handleFileUpload}
+        />
+        {/* 닉네임 입력 */}
+        <div className="flex flex-col gap-4 mt-10">
+          <label
+            htmlFor="nickname"
+            className="font-normal text-2xl flex space-x-1"
+          >
+            <span>닉네임</span>
+            <Required />
+          </label>
+          <div className="flex flex-col gap-3">
+            <div className="border-2 border-border  rounded-xl focus-within:border-primary flex justify-between px-4  py-6">
+              <input
+                id="nickname"
+                type="text"
+                name="nickname"
+                autoComplete="off"
+                placeholder="닉네임을 입력해주세요."
+                onChange={onChange}
+                value={nickname}
+                className="w-full px-4 text-2xl rounded-xl outline-none font-medium leading-8"
+              />
+              <div className="flex space-x-2">
+                {isNicknameValid && isNicknameCheckSuccess && (
+                  <div className="min-w-min">
+                    <Checked className="w-7 h-7" />
+                  </div>
+                )}
+                {nickname.length > 0 && (
+                  <XMarkSVG
+                    onClick={() => {
+                      setNickname('')
+                      setNicknameErrorMessage(null)
+                      setIsNicknameValid(false)
+                      setIsNicknameCheckSuccess(false)
+                    }}
+                    className="w-7 h-7 cursor-pointer"
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        {nicknameErrorMessage && (
+          <p className="whitespace-pre-wrap text-warning pl-4">
+            {nicknameErrorMessage}
+          </p>
+        )}
+        {isNicknameCheckSuccess && (
+          <p className="whitespace-pre-wrap text-success pl-4">
+            사용 가능한 닉네임입니다.
+          </p>
+        )}
+        <button
+          type="button"
+          className="w-full py-7 text-2xl font-medium bg-primary rounded-xl mt-[11.25rem]"
+          onClick={() => router.push('/mypage')}
+        >
+          저장
+        </button>
+      </div>
+      {isModalOpen && (
+        <div
+          role="presentation"
+          className="w-full h-full  flex-col   bg-[rgba(76,76,76,0.8)] fixed top-0 left-0 z-50 flex justify-end items-center pb-20 gap-6"
+          onClick={handleModalBackgroundClick}
+        >
+          <div
+            role="presentation"
+            className="w-[31.25rem] h-[10rem] divide-y divide-solid bg-white rounded-xl text-xl font-medium leading-7"
+          >
+            <label
+              role="presentation"
+              htmlFor="file-input"
+              onClick={handleProfileImageChangeClick}
+              className="w-full h-[5rem] flex items-center justify-center cursor-pointer"
+            >
+              <span>프로필 이미지 수정</span>
+            </label>
+            <button
+              type="button"
+              className="w-full h-[5rem]"
+              onClick={() => {
+                setImageUrl(null)
+                closeModal()
+              }}
+            >
+              기본 이미지로 변경
+            </button>
+          </div>
+          <button
+            type="button"
+            className="w-[31.25rem] h-[5rem] bg-white rounded-xl text-warning text-xl font-medium leading-7"
+            onClick={closeModal}
+          >
+            취소
+          </button>
+        </div>
+      )}
+    </>
+  )
+}
