952e525cb0795eb35ef55523406bef58186b9248
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-02 15:00:26 +0900

    design: 비밀번호 재설정 이메일 인증 페이지 디자인 구현

---
+++ src/app/(auth)/login/password_reset/new/page.tsx
@@ -0,0 +1,11 @@
+export default function PasswordResetNew() {
+  return (
+    <div className="px-7 pt-[11rem] relative">
+      <div className="flex flex-col gap-10">
+        <h2 className="text-[2rem] font-medium">
+          새로운 비밀번호를 설정해주세요.
+        </h2>
+      </div>
+    </div>
+  )
+}
