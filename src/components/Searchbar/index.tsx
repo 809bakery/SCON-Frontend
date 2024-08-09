39c83b5302eaf35cbcf1dff916af5f6d7ceb743e
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-09 18:16:45 +0900

    feat: 검색바에 디바운스 커스텀 훅 적용

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -2,17 +2,33 @@
 
 'use client'
 
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+import useDebounce from '@/hooks/useDebounce.ts'
 import SearchSVG from '@/static/svg/search-icon.svg'
 
-interface SearchBarProps {
-  searchQuery: string | undefined
-  setSearchQuery: (value: string) => void
-}
+export default function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState('')
+  const { replace } = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const debouncedValue = useDebounce<string>(searchQuery, 200)
 
-export default function SearchBar({
-  searchQuery,
-  setSearchQuery,
-}: SearchBarProps) {
+  useEffect(() => {
+    function handleSearch(keyword: string) {
+      const params = new URLSearchParams(searchParams)
+      if (keyword) {
+        params.set('keyword', keyword)
+      } else {
+        params.delete('keyword')
+      }
+
+      replace(`${pathname}?${params.toString()}`)
+    }
+    handleSearch(debouncedValue)
+  }, [debouncedValue, pathname, replace, searchParams])
+
   return (
     <div className="w-full border-2 border-border  rounded-xl focus-within:border-primary flex justify-between items-center px-3  py-3">
       <input
