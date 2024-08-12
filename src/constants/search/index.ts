49c63c66fd91307c3a0efa2f36712fc20f00b9b9
kwakseongjae <gkffhdnls13@gmail.com>
2024-08-12 21:42:56 +0900

    chore: enum활용 더미변수 구체화

--- src/constants/search/index.ts
+++ src/constants/search/index.ts
@@ -1,5 +1,6 @@
 import { StaticImageData } from 'next/image'
 
+import { StageCategory } from '@/features/event/types/StageCategory.ts'
 import DummyOvenProfile1 from '@/static/img/dummy/search/dummy-oven-profile1.jpg'
 import DummyOvenProfile2 from '@/static/img/dummy/search/dummy-oven-profile2.jpg'
 import DummyOvenProfile3 from '@/static/img/dummy/search/dummy-oven-profile3.jpg'
@@ -35,7 +36,7 @@ export type StageType = {
   location: string
   time: string
   image: string | StaticImageData
-  category?: string
+  category: StageCategory
 }
 
 export const SearchFilterList: FilterType[] = [
@@ -140,7 +141,7 @@ export const StageList: StageType[] = [
     location: '소환사의 협곡 바텀 라인',
     time: '2024. 7. 22 ~ 7. 25',
     image: DummyStageProfile1,
-    category: '소모임',
+    category: StageCategory.club,
   },
   {
     id: 2,
@@ -148,7 +149,7 @@ export const StageList: StageType[] = [
     location: '경희대학교 평화의 전당',
     time: '2024. 7. 12 ~ 7. 13',
     image: DummyStageProfile2,
-    category: '공연',
+    category: StageCategory.performance,
   },
   {
     id: 3,
@@ -156,7 +157,7 @@ export const StageList: StageType[] = [
     location: '잠실 롯데시네마 7층',
     time: '2024. 7. 22 ~ 7. 25',
     image: DummyStageProfile3,
-    category: '공연',
+    category: StageCategory.performance,
   },
   {
     id: 4,
@@ -164,6 +165,6 @@ export const StageList: StageType[] = [
     location: '인스파이어 아레나',
     time: '2024. 8.25',
     image: DummyStageProfile4,
-    category: '기타',
+    category: StageCategory.etc,
   },
 ]
