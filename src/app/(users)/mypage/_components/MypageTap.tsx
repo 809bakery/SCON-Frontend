97bdba4c3470d76dfcff8016017831cdb3ec08d9
하림 <ez703@naver.com>
2024-08-13 17:34:01 +0900

    feat: props로 아이콘 추가!!

--- src/app/(users)/mypage/_components/MypageTap.tsx
+++ src/app/(users)/mypage/_components/MypageTap.tsx
@@ -1,14 +1,20 @@
 import { useRouter } from 'next/navigation'
 
 import ExitSVG from '@/static/svg/exit-icon.svg'
+import NotificationSVG from '@/static/svg/notification-setting-icon.svg'
+import PasswordResetSVG from '@/static/svg/password-reset-icon.svg'
+import ProfileSettingSVG from '@/static/svg/profile-setting-icon.svg'
+import TermSVG from '@/static/svg/terms-icon.svg'
+import MyOvenSVG from '@/static/svg/my-oven-icon.svg'
 
 interface MypageTapProps {
   text?: string
   url?: string
   color?: string
+  icon?: string
 }
 
-export default function MypageTap({ text, url, color }: MypageTapProps) {
+export default function MypageTap({ text, url, color, icon }: MypageTapProps) {
   const router = useRouter()
 
   const handleUrl = () => {
@@ -27,7 +33,17 @@ export default function MypageTap({ text, url, color }: MypageTapProps) {
       style={{ color: color ? `${color}` : '#000' }}
       onClick={handleUrl}
     >
-      <ExitSVG className="w-20 h-20" />
+      {icon === 'exit' && <ExitSVG className="w-20 h-20" />}
+      {icon === 'my-oven' && <MyOvenSVG className="w-20 h-20" />}
+      {icon === 'notification-setting' && (
+        <NotificationSVG className="w-20 h-20" />
+      )}
+      {icon === 'profile-setting' && (
+        <ProfileSettingSVG className="w-20 h-20" />
+      )}
+      {icon === 'password-reset' && <PasswordResetSVG className="w-20 h-20" />}
+      {icon === 'term' && <TermSVG className="w-20 h-20" />}
+
       <span className="text-xl font-medium">{text}</span>
     </div>
   )
